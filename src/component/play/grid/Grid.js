02c9573f6c34faa8998fa5f0b1b2c77efd5c68e0
MoxiaoHe <he.moxiao@gmail.com>
2020-11-10 22:04:58 -0800

    add alert

--- src/component/play/grid/Grid.js
+++ src/component/play/grid/Grid.js
@@ -1,134 +1,142 @@
-import React, { useEffect, useState } from "react";
-import { connect } from "react-redux";
-import Cell from "./cell/Cell";
-import { operationState, direction as dir } from "../../constant/Constant";
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import Cell from './cell/Cell';
+import { operationState, direction as dir } from '../../constant/Constant';
 
-const Grid = (props) => {
-  const {
-    gridSize,
-    isActive,
-    operation,
-    intervalTime,
-    updateCellNumber,
-    isHeatmap,
-    setDefaultOperationHandler,
-  } = props;
-  const [grid, setGrid] = useState(initGrid());
+const Grid = props => {
+    const {
+        gridSize,
+        isActive,
+        operation,
+        intervalTime,
+        updateCellNumber,
+        isHeatmap,
+        setDefaultOperationHandler,
+    } = props;
+    const [grid, setGrid] = useState(initGrid());
 
-  const nextGenerationHandler = () => {
-    const newGrid = [...grid];
+    const nextGenerationHandler = () => {
+        const newGrid = [...grid];
 
-    for (let i = 0; i < grid.length; i++) {
-      for (let j = 0; j < grid.length; j++) {
-        let liveNeighbor = 0;
-        for (let k = 0; k < dir.length; k++) {
-          let row = i + dir[k][0];
-          let col = j + dir[k][1];
-          if (row < 0 || row >= grid.length || col < 0 || col >= grid.length) {
-            continue;
-          }
-          if (grid[row][col] === 0) {
-            liveNeighbor += 1;
-          }
+        for (let i = 0; i < grid.length; i++) {
+            for (let j = 0; j < grid.length; j++) {
+                let liveNeighbor = 0;
+                for (let k = 0; k < dir.length; k++) {
+                    let row = i + dir[k][0];
+                    let col = j + dir[k][1];
+                    if (
+                        row < 0 ||
+                        row >= grid.length ||
+                        col < 0 ||
+                        col >= grid.length
+                    ) {
+                        continue;
+                    }
+                    if (grid[row][col] === 0) {
+                        liveNeighbor += 1;
+                    }
+                }
+
+                if (
+                    grid[i][j] === 0 &&
+                    (liveNeighbor < 2 || liveNeighbor > 3)
+                ) {
+                    newGrid[i][j] = 1;
+                } else if (grid[i][j] > 0 && liveNeighbor === 3) {
+                    newGrid[i][j] = 0;
+                } else if (grid[i][j] > 0) {
+                    newGrid[i][j] = grid[i][j] + 1;
+                }
+            }
         }
-
-        if (grid[i][j] === 0 && (liveNeighbor < 2 || liveNeighbor > 3)) {
-          newGrid[i][j] = 1;
-        } else if (grid[i][j] > 0 && liveNeighbor === 3) {
-          newGrid[i][j] = 0;
-        } else if (grid[i][j] > 0) {
-          newGrid[i][j] = grid[i][j] + 1;
-        }
-      }
-    }
-    setGrid(newGrid);
-  };
-
-  const onClickHandler = (rowIdx, colIdx) => {
-    if (!isActive) {
-      const newGrid = [...grid];
-      newGrid[rowIdx][colIdx] = grid[rowIdx][colIdx] === 0 ? 1 : 0;
-      setGrid(newGrid);
-    }
-  };
-
-  function initGrid() {
-    const cells = [];
-    for (let i = 0; i < gridSize; i += 1) {
-      let row = [];
-      for (let j = 0; j < gridSize; j += 1) {
-        const cell = Math.random() < 0.5 ? 0 : 1;
-        row.push(cell);
-      }
-      cells.push(row);
-    }
-
-    return cells;
-  }
-
-  useEffect(() => {
-    let interval = null;
-    if (operation === operationState.START) {
-      interval = setInterval(nextGenerationHandler, intervalTime);
-    } else if (operation === operationState.PAUSE) {
-      if (interval) {
-        clearInterval(interval);
-      }
-    } else if (operation === operationState.RESET) {
-      setGrid(initGrid());
-      if (interval) {
-        clearInterval(interval);
-      }
-      setDefaultOperationHandler();
-    }
-
-    return () => {
-      if (interval) {
-        clearInterval(interval);
-      }
+        setGrid(newGrid);
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [operation]);
 
-  useEffect(() => {
-    let count = 0;
-    for (let i = 0; i < grid.length; i += 1) {
-      for (let j = 0; j < grid.length; j += 1) {
-        if (grid[i][j] === 0) {
-          count++;
+    const onClickHandler = (rowIdx, colIdx) => {
+        if (!isActive) {
+            const newGrid = [...grid];
+            newGrid[rowIdx][colIdx] = grid[rowIdx][colIdx] === 0 ? 1 : 0;
+            setGrid(newGrid);
         }
-      }
-    }
-    updateCellNumber(count);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [grid]);
-  const wrapperStyle = {
-    display: "grid",
-    gridTemplateColumns: `repeat(${gridSize}, 1fr)`,
-    gridTemplateRows: `repeat(${gridSize}, 1fr)`,
-  };
+    };
 
-  return (
-    <div style={wrapperStyle}>
-      {grid.map((row, rowIdx) =>
-        row.map((cell, colIdx) => (
-          <Cell
-            clicked={onClickHandler.bind(this, rowIdx, colIdx)}
-            key={`${rowIdx},${colIdx}`}
-            isHeatmap={isHeatmap}
-            gradient={cell}
-          />
-        ))
-      )}
-    </div>
-  );
+    function initGrid() {
+        const cells = [];
+        for (let i = 0; i < gridSize; i += 1) {
+            let row = [];
+            for (let j = 0; j < gridSize; j += 1) {
+                const cell = Math.random() < 0.05 ? 0 : 1;
+                row.push(cell);
+            }
+            cells.push(row);
+        }
+
+        return cells;
+    }
+
+    useEffect(() => {
+        let interval = null;
+        if (operation === operationState.START) {
+            interval = setInterval(nextGenerationHandler, intervalTime);
+        } else if (operation === operationState.PAUSE) {
+            if (interval) {
+                clearInterval(interval);
+            }
+        } else if (operation === operationState.RESET) {
+            setGrid(initGrid());
+            if (interval) {
+                clearInterval(interval);
+            }
+            setDefaultOperationHandler();
+        }
+
+        return () => {
+            if (interval) {
+                clearInterval(interval);
+            }
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [operation]);
+
+    useEffect(() => {
+        let count = 0;
+        for (let i = 0; i < grid.length; i += 1) {
+            for (let j = 0; j < grid.length; j += 1) {
+                if (grid[i][j] === 0) {
+                    count++;
+                }
+            }
+        }
+        updateCellNumber(count);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [grid]);
+    const wrapperStyle = {
+        display: 'grid',
+        gridTemplateColumns: `repeat(${gridSize}, 1fr)`,
+        gridTemplateRows: `repeat(${gridSize}, 1fr)`,
+    };
+
+    return (
+        <div style={wrapperStyle}>
+            {grid.map((row, rowIdx) =>
+                row.map((cell, colIdx) => (
+                    <Cell
+                        clicked={onClickHandler.bind(this, rowIdx, colIdx)}
+                        key={`${rowIdx},${colIdx}`}
+                        isHeatmap={isHeatmap}
+                        gradient={cell}
+                    />
+                ))
+            )}
+        </div>
+    );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    gridSize: state.gridSize,
-    intervalTime: state.interval,
-  };
+const mapStateToProps = state => {
+    return {
+        gridSize: state.gridSize,
+        intervalTime: state.interval,
+    };
 };
 
 export default connect(mapStateToProps, null)(Grid);
--- src/component/setting/Setting.js
+++ src/component/setting/Setting.js
@@ -19,14 +19,29 @@ const Setting = props => {
     };
 
     const buttonOnClickHandler = () => {
-        changePage(1);
         changeGridSize(gridSize);
         changeInterval(interval);
+        if (gridSize < 10 || gridSize > 100) {
+            alert('The grid size range need to be between 10 to 100.');
+            changeGridSize(10);
+            changeInterval(200);
+            return;
+        }
+
+        if (interval < 50 || gridSize > 2000) {
+            alert(
+                'The frequency of a generation need to be between 50 to 2000ms.'
+            );
+            changeGridSize(10);
+            changeInterval(200);
+            return;
+        }
+        changePage(1);
     };
 
     return (
         <div className={styles['setting-wrapper']}>
-            <label className={styles.label}>Grid Size</label>
+            <label className={styles.label}>Grid Size (10 - 100)</label>
             <input
                 className={styles.input}
                 type="number"
@@ -34,7 +49,7 @@ const Setting = props => {
                 onChange={gridSizeChangeHandler}
             />
             <label className={styles.label}>
-                Generation Interval Time (ms)
+                Generation Interval Time (50 - 2000ms)
             </label>
             <input
                 className={styles.input}
